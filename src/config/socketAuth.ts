3ecf18624cb5176e76bfa7e2fc93f9613933ba2f
brijbihari24 <awadhiyabrijbihari@gmail.com>
2026-09-23 17:42:16 +0530

    feat: add socket authentication middleware and integrate Yjs for real-time collaboration

--- src/config/socketAuth.ts
+++ src/config/socketAuth.ts
@@ -2,18 +2,31 @@ import { Socket } from "socket.io";
 import jwt from "jsonwebtoken"
 
 interface AuthSocket extends Socket {
-    userId: string
+    userId?: string
 }
 
 export const socketAuthMiddleware = (socket: AuthSocket, next: (err?: Error) => (void)) => {
-    // step1. check if token is present
+    // step1. here we are taking token out
     const token = socket.handshake.auth?.token
 
+    // checking token 
     if (!token) {
         return (
             next(new Error("Authentication Required"))
         )
     }
 
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string }
+        socket.userId = decoded.userId
+        next()
+    } catch (error) {
+        return (
+            next(new Error("Invalid or Expired Token"))
+        )
+    }
+
 }
+
+export type { AuthSocket };
 
--- src/server.ts
+++ src/server.ts
@@ -8,25 +8,45 @@ import "dotenv/config";
 import { createServer } from "http"
 import { Server } from "socket.io"
 import app from "./app.js"
+import { socketAuthMiddleware, type AuthSocket } from "./config/socketAuth.js"
 
 
 // create server 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        origin: [
+            process.env.CLIENT_URL || "http://localhost:5173",
+            "http://localhost:5500",  // test file ka origin
+            "http://127.0.0.1:5500"
+        ],
         methods: ["GET", "POST"]
     }
 })
 
-//configure socket io
-io.on("connection", (socket) => {
-    console.log(`user with socket id ${socket.id} is connected`);
+io.use(socketAuthMiddleware)
 
-    socket.on("disconnected", () => {
-        console.log(`user with socket id ${socket.id} is disconnected`);
+//configure socket io
+io.on("connection", (socket: AuthSocket) => {
+    console.log(`New socket connected with socketId: ${socket.id}`);
+
+    socket.on("join-board", (boardId: string) => {
+        socket.join(boardId)
+        console.log(`socket with socketId ${socket.id} joined board with boardId ${boardId}`);
+
     })
+
+    socket.on("leave-board", (boardId: string) => {
+        socket.leave(boardId)
+    })
+
+    socket.on("disconnect", () => {
+        console.log(`Socket/User with ${socket.userId} disconnected`);
+
+    })
+
 })
+
 
 const PORT = process.env.PORT || 4000;
 
